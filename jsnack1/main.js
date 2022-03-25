4b30fa5b83dcfa2c3cd2e8a2f756c328ce4225ff
simone <mondellosimo@gmail.com>
2022-03-25 14:31:26 +0100

    commit.esercizio1.finito

---
+++ jsnack1/main.js
@@ -0,0 +1,10 @@
+let numero1 = parseInt(prompt('inserisci primo numero'));
+let numero2 = parseInt(prompt('inserisci secondo numero '));
+
+let numeroMaggiore= [numero1, numero2];
+let m = Math.max(numeroMaggiore);
+console.log(numeroMaggiore);
+
+
+
+
