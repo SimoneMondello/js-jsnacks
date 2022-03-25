3b7d8cadde8055f672484de9fd8e2e6446fc65c4
simone <mondellosimo@gmail.com>
2022-03-25 16:19:18 +0100

    sesto.esercizio.commit

---
+++ jsnack6/main.js
@@ -0,0 +1,30 @@
+
+
+let computer1=Math.round(Math.random()*10+1);
+    console.log(computer1);
+		
+let numero = parseInt(prompt('inserisci numero da 1 a 10'));
+console.log(numero);
+	
+
+	
+    
+    if (computer1>numero)
+    {
+     alert("hai perso");
+     prompt('Riprova')
+    }
+    
+    if 
+    (numero<computer1) 
+    {
+	    alert("hai perso");
+        prompt('Riprova')
+    }
+    if
+	  (numero==computer1);
+    {
+    alert('hai vinto');
+    } 
+  
+console.log();
