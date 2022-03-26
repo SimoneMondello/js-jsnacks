4dacccccdcbd9029351768d4ff8635c6e36b20c2
simone <mondellosimo@gmail.com>
2022-03-26 17:45:27 +0100

    esercizio.otto

---
+++ jsnack8/main.js
@@ -0,0 +1,19 @@
+
+
+let listanumeri = [10,2,3,4,5,6,1,5,5];
+console.log(listanumeri);
+
+sommaNumeri();
+
+    function sommaNumeri ()
+    {
+    let somma=0;
+    for (let i=0; i < listanumeri.length; i++)
+    {
+        somma += listanumeri[i];
+    }
+     console.log(somma);
+    }
+    
+
+
